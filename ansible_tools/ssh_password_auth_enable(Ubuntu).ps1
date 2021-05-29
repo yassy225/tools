6a906d3dfc3e2ddee936b54dcf79bc332a28adda
yassy225 <yassy.3375@gmail.com>
2021-05-29 12:30:23 +0000

    Create new

---
+++ ansible_tools/ssh_password_auth_enable(Ubuntu).ps1
@@ -0,0 +1,6 @@
+$file = ".\Vagrantfile"
+$data = Get-Content $file
+$length = $data.length -1
+$data[$length] = $null
+$data=$data+"  config.vm.provision 'shell', inline: <<-SHELL`r`n      sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config`r`n      sudo service sshd restart`r`n  SHELL`r`nend"
+$data | Out-File $file -Encoding UTF8
