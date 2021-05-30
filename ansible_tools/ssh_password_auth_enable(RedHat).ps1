6b8452fea93a51fc9d1c5e4756c499bd382a43e3
yassy225 <yassy.3375@gmail.com>
2021-05-30 03:36:54 +0000

    Edit script

--- ansible_tools/ssh_password_auth_enable(RedHat).ps1
+++ ansible_tools/ssh_password_auth_enable(RedHat).ps1
@@ -2,5 +2,5 @@ $file = ".\Vagrantfile"
 $data = Get-Content $file
 $length = $data.length -1
 $data[$length] = $null
-$data=$data+"  config.vm.provision 'shell', inline: <<-SHELL`r`n      sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config`r`n      sudo systemctl restart sshd`r`n  SHELL`r`nend"
+$data=$data+"  config.vm.provision 'shell', inline: <<-SHELL`r`n      sudo sed -i -e 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config`r`n      sudo systemctl restart sshd`r`n  SHELL`r`nend"
 $data | Out-File $file -Encoding UTF8
